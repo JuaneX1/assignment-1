a0b5449684017b31cb0948afce42f13085159b81
Juan <penuelajuanesteban30@gmail.com>
2024-08-20 17:40:35 -0400

    feat: translating files to typescript

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,14 +2,23 @@
 
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: { params: { num: string } }, res: { send: (result: string) => void }) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const numAsNumber = parseInt(num, 10);
+
+  if (isNaN(numAsNumber)) {
+    res.send(`Invalid input. Please provide a valid number.`);
+    return;
+  }
+
+  const fibN = fibonacci(numAsNumber);
+  let result: string;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
+  } else {
+    result = `fibonacci(${num}) is ${fibN}`;
   }
 
   res.send(result);
